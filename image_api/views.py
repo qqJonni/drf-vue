affa5bf55c958a63deac071706ae06370368f8c3
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-18 15:15:14 +0400

    добавил возможность отобразить, создать и удалить данные с БД

--- image_api/views.py
+++ image_api/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 from .serializers import ImageSerializer
 from rest_framework.viewsets import ModelViewSet
 from image_api.models import Image
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class ImageModelViewSet(ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 def images_page(request):
--- static/app.js
+++ static/app.js
@@ -1,7 +1,15 @@
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+
+
 new Vue({
     el: '#images_app',
     data: {
-        images: []
+        images: [],
+        newImage: {
+            description: '',
+            image_data: ''
+        }
     },
     methods: {
         requestData: function () {
@@ -12,6 +20,34 @@ new Vue({
                 })
                 .catch(function (error) {
                     console.error('Error fetching data:', error);
+                });
+        },
+        addImageData: function () {
+            const vm = this;
+            // Отправка данных на сервер для добавления в базу данных
+            axios.post('/api/images/', vm.newImage)
+                .then(function (response) {
+                    // Обновление списка изображений после успешного добавления
+                    vm.requestData();
+                    // Очистка формы
+                    vm.newImage = {
+                        description: '',
+                        image_data: ''
+                    };
+                })
+                .catch(function (error) {
+                    console.error('Error adding data:', error);
+                });
+        },
+        deleteImageData: function (imageId) {
+            const vm = this;
+            axios.delete(`/api/images/${imageId}/`)
+                .then(function (response) {
+                    // Обновление списка изображений после успешного удаления
+                    vm.requestData();
+                })
+                .catch(function (error) {
+                    console.error('Error deleting data:', error);
                 });
         }
     }
